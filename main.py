c1c33a2d5a842f5b8b4b91ca51ae633b4eb2fcba
Mario Torres <48029021+marito07@users.noreply.github.com>
2021-08-25 14:50:31 +0200

    It kinda works now

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import re
 import time
 import string
+import os
 
 
 #set the headers as a browser
@@ -33,19 +34,56 @@ while True:
     response = requests.get(url, headers=headers)
     #parse the downloaded homepage and grab all text
     soup = BeautifulSoup(response.text, "lxml")
-    last_movie = soup.find("h3", {"class": "headline-3 prettify"})
+    last_movie = soup.find("tr", {"class": "diary-entry-row"})
     if previous_movie != '' and last_movie != previous_movie:
-        file = open('temp.txt', 'w')
-        file.write(listIndex + '. ' +  + ')\n')
-        tweett = api.update_status(status=last_movie.text, 
+        last_movie_text = last_movie.find("h3", {"class": "headline-3 prettify"})
+        rating = last_movie.find("span", {"class": "rating"})
+        movie_url =  last_movie_text.find("a")
+        movie_url_array = movie_url['href'].split('/')
+        del movie_url_array[0]
+        del movie_url_array[0]
+        if movie_url_array[-2].isnumeric():
+            del movie_url_array[-2]
+        response2 = requests.get('https://letterboxd.com/' + '/'.join(movie_url_array), headers=headers)
+        soup2 = BeautifulSoup(response2.text, "lxml")
+        last_movie_img = soup2.find("img", {"class": "image"})
+        movie_year = soup2.find("small", {"class": "number"})
+        film_header = soup2.find("section", {"class": "film-header-lockup"})
+        directors = film_header.find_all("span", {"class": "prettify"})
+        directors_text = ''
+        for dir in directors:
+            directors_text += dir.text
+            if directors.index(dir) != len(directors)-2 and len(directors) != 1:
+                directors_text += ', '
+            if directors.index(dir) == len(directors)-2 and len(directors) != 1:
+                directors_text += ' & '
+        urlBoxId = soup2.find("input", {"class": "field -transparent"})
+        print(directors_text)
+        print(last_movie_text)
+        print(last_movie_img['srcset'])
+        print(urlBoxId['value'])
+        filename = 'temp.jpg'
+        requestImage = requests.get(last_movie_img['srcset'], stream=True)
+        with open(filename, 'wb') as image:
+            for chunk in requestImage:
+                image.write(chunk)
+        lines = []
+        lines.append(str(listIndex) + '. ' + last_movie_text.text + ' (' + movie_year.text + ')')
+        listIndex = listIndex + 1
+        lines.append('Dir: ' + directors_text)
+        lines.append('')
+        lines.append(rating.text.strip())
+        lines.append('')
+        lines.append(urlBoxId['value'])
+        multiline_tweet = "\n".join(lines)
+        tweett = api.update_with_media(filename, status=multiline_tweet, 
                                  in_reply_to_status_id=twitterThreadID, 
                                  auto_populate_reply_metadata=True)
         twitterThreadID = tweett.id
-        ++listIndex
     else:
         print('No new movie')
     previous_movie = last_movie
     #previous_movie_rating = soup.find("span", {"class": "rating rated-5"})
 
-    time.sleep(10)
+    time.sleep(3)
     
