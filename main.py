1fd6e45f6127c4231daf89f69b60117f470befad
Mario Torres <48029021+marito07@users.noreply.github.com>
2022-02-22 19:54:32 +0100

    fixing the fix

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ while True:
         
         multiline_tweet = "\n".join(lines)
         # Sends Tweet
-        tweett = api.update_with_media(filename, status=multiline_tweet, 
+        tweett = api.update_with_media(filename2, status=multiline_tweet, 
                                   in_reply_to_status_id=twitterThreadID, 
                                  auto_populate_reply_metadata=True)        
         twitterThreadID = tweett.id
