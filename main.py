c08ca7ebc55be44b2cab75b6cc68283d4b3a0b7c
Mario Torres <48029021+marito07@users.noreply.github.com>
2022-11-27 21:23:18 +0100

    Test3

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ dotenv.load_dotenv()
 
 
 # Get port number from the PORT environment varaible or 3000 if not specified
-port = os.getenv('PORT', 3000)
+port = os.getenv('PORT', '3000')
 server = HTTPServer(('0.0.0.0', port), MyServer)
 server.serve_forever()
 
